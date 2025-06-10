75519b1b9e2adbb617d74edacf1fd3826edfe59b
tejas-teju <tejas8196@gmail.com>
2025-06-11 02:26:26 +0530

    Add auth routes

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/api/auth/signup", (req, res) => {
+  res.send("Signup route");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  res.send("Login route");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  res.send("Logout route");
 });
 
 app.listen(PORT, () => {
