952adfb7a595e2048be7564e9a7acb8e6c025782
tejas-teju <tejas8196@gmail.com>
2025-06-11 02:24:30 +0530

    Get port from .env file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
